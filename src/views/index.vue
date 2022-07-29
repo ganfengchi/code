b14fd36db5d8e45c2c9ee334ffc9674a850cdddc
ganfengchi <ganfengchi521@163.com>
2022-07-29 16:46:17 +0800

    右侧边栏汉化 已经xml-loader

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,54 +1,54 @@
 <template>
-    <div id="app">
-        <div class="tool">
-            <span>
-                <a-upload name="file" :headers="headers" @change="handleChange">
-                    <a-button>
-                        <a-icon type="upload" /> Click to Upload
-                    </a-button>
-                </a-upload>
-            </span>
-            <a-button type="primary" class="new" icon="plus" @click="newDiagram"></a-button>
-            <a-button type="primary" icon="download" @click="downloadBpmn"></a-button>
-            <a-button type="primary" icon="picture" @click="downloadSvg"></a-button>
-        </div>
-        <div class="container">
-            <!-- 创建一个canvas画布 npmn-js是通过canvas实现绘图的，并设置ref让vue获取到element -->
-            <div class="bpmn-canvas" ref="canvas"></div>
-            <div class="bpmn-js-properties-panel" id="js-properties-panel"></div>
-        </div>
+  <div id="app">
+    <div class="tool">
+      <span>
+        <a-upload name="file" :headers="headers" @change="handleChange">
+          <a-button>
+            <a-icon type="upload" />Click to Upload
+          </a-button>
+        </a-upload>
+      </span>
+      <a-button type="primary" class="new" icon="plus" @click="newDiagram">reset</a-button>
+      <a-button type="primary" icon="download" @click="downloadBpmn">download</a-button>
+      <a-button type="primary" icon="picture" @click="downloadSvg">downloadSvg</a-button>
     </div>
+    <div class="container">
+      <!-- 创建一个canvas画布 npmn-js是通过canvas实现绘图的，并设置ref让vue获取到element -->
+      <div class="bpmn-canvas" ref="canvas"></div>
+      <div class="bpmn-js-properties-panel" id="js-properties-panel"></div>
+    </div>
+  </div>
 </template>
 
 
   <script>
 // 引入相关的依赖
-import 'bpmn-js/dist/assets/diagram-js.css'
-import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css'
-import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css'
-import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css'
+import "bpmn-js/dist/assets/diagram-js.css";
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn.css";
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css";
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
 // 在这里引入一下Bpmn建模器对象
 import BpmnModeler from "bpmn-js/lib/Modeler";
 
 // 左边工具栏以及编辑节点的样式
-import 'bpmn-js-properties-panel/dist/assets/bpmn-js-properties-panel.css'
+import "bpmn-js-properties-panel/dist/assets/bpmn-js-properties-panel.css";
 
 // 工具栏相关
 import propertiesProviderModule from "bpmn-js-properties-panel/lib/provider/camunda";
 import propertiesPanelModule from "bpmn-js-properties-panel";
 import camundaModdleDescriptor from "camunda-bpmn-moddle/resources/camunda";
-
+import customTranslate from './customTranslate'
 
 export default {
-    data() {
-        return {
-            headers: {
-                authorization: 'authorization-text',
-            },
-            bpmnModeler: null,
-            canvas: null,
-            // 这部分具体的代码我放到了下面
-            initTemplate: `<?xml version="1.0" encoding="UTF-8"?>
+  data() {
+    return {
+      headers: {
+        authorization: "authorization-text"
+      },
+      bpmnModeler: null,
+      canvas: null,
+      // 这部分具体的代码我放到了下面
+      bpmnTemplate: `<?xml version="1.0" encoding="UTF-8"?>
 <definitions 
   xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL" 
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
@@ -80,130 +80,174 @@ export default {
     </bpmndi:BPMNPlane>
   </bpmndi:BPMNDiagram>
 </definitions>`
-        };
+    };
+  },
+  methods: {
+    init() {
+      // 获取画布 element
+      this.canvas = this.$refs.canvas;
+      const  customTranslateModule = {
+        translate: ["value", customTranslate] //汉化
+      };
+      // 创建Bpmn对象
+      this.bpmnModeler = new BpmnModeler({
+        // 设置bpmn的绘图容器为上门获取的画布 element
+        container: this.canvas,
+
+        // 加入工具栏支持
+        propertiesPanel: {
+          parent: "#js-properties-panel"
+        },
+        //右边属性侧边栏
+        additionalModules: [
+          propertiesProviderModule, ////左边攻击巨烂以及节点
+          propertiesPanelModule, //右边的工具栏
+          customTranslateModule //汉化
+        ],
+        moddleExtensions: {
+          camunda: camundaModdleDescriptor
+        }
+      });
+
+      this.createNewDiagram(this.bpmnTemplate);
     },
-    methods: {
-       
-
-        init() {
-            // 获取画布 element
-            this.canvas = this.$refs.canvas;
-
-            // 创建Bpmn对象
-            this.bpmnModeler = new BpmnModeler({
-                // 设置bpmn的绘图容器为上门获取的画布 element
-                container: this.canvas,
-
-                // 加入工具栏支持
-                propertiesPanel: {
-                    parent: "#js-properties-panel"
-                },
-                additionalModules: [propertiesProviderModule, propertiesPanelModule],
-                moddleExtensions: {
-                    camunda: camundaModdleDescriptor
-                }
-            });
-
-            this.createNewDiagram(this.initTemplate);
-        },
-        // initDiagram(bpmn) {
-        //     // 将xml导入Bpmn-js建模器
-        //     this.bpmnModeler.importXML(bpmn, err => {
-        //         if (err) {
-        //             this.$Message.error("打开模型出错,请确认该模型符合Bpmn2.0规范");
-        //         }
-        //     });
-        // },
-        async createNewDiagram(bpmn) {
-            // 将字符串转换成图显示出来;
-            this.bpmnModeler.importXML(bpmn, err => {
-                if (err) {
-                    this.$Message.error("打开模型出错,请确认该模型符合Bpmn2.0规范");
-                }
-            });
-        },
-         openBpmn(file) {
-            const reader = new FileReader();
-            // 读取File对象中的文本信息，编码格式为UTF-8
-            reader.readAsText(file, "utf-8");
-            reader.onload = () => {
-                //读取完毕后将文本信息导入到Bpmn建模器
-                this.createNewDiagram(reader.result);
-            };
-            return false;
-        },
-        newDiagram() {
-            this.createNewDiagram(this.bpmnTemplate);
-        },
-        downloadBpmn() {
-            this.bpmnModeler.saveXML({ format: true }, (err, xml) => {
-                if (!err) {
-                    console.log(xml,'xml');
-                    // 获取文件名
-                    const name = `${this.getFilename(xml)}.bpmn`;
-                    // 将文件名以及数据交给下载函数
-                    this.download({ name: name, data: xml });
-                }
-            });
-        },
-        download({ name = "diagram.bpmn", data }) {
-            const blob=new Blob([data])
-            console.log('--------------',blob);
-            // 这里就获取到了之前设置的隐藏链接
-            const downloadLink = this.$refs.downloadLink;
-            // 把数据转换为URI，下载要用到的
-            const encodedData = encodeURIComponent(data,{type:'data:text/xml;charset=utf-8'});
-            // if (data) {
-            //     // 将数据给到链接
-            //     downloadLink.href =
-            //         "data:application/bpmn20-xml;charset=UTF-8," + encodedData;
-            //     // 设置文件名
-            //     downloadLink.download = name;
-            //     // 触发点击事件开始下载
-            //     downloadLink.click();
-            // }
-        },
-
-        getFilename(xml) {
-            let start = xml.indexOf("process");
-            let filename = xml.substr(start, xml.indexOf(">"));
-            filename = filename.substr(filename.indexOf("id") + 4);
-            filename = filename.substr(0, filename.indexOf('"'));
-            return filename;
-        },
-        handleChange(info) {
-            if (info.file.status !== 'uploading') {
-                console.log(info.file, info.fileList);
-            }
-            if (info.file.status === 'done') {
-                this.$message.success(`${info.file.name} file uploaded successfully`);
-            } else if (info.file.status === 'error') {
-                this.$message.error(`${info.file.name} file upload failed.`);
-            }
-        },
+    async createNewDiagram(bpmn) {
+      // 将字符串转换成图显示出来;
+      this.bpmnModeler.importXML(bpmn, err => {
+        if (err) {
+          this.$Message.error("打开模型出错,请确认该模型符合Bpmn2.0规范");
+        }
+      });
     },
-    // 生命周期钩子，在组件加载完成后调用init函数进行创建初始化Bpmn-js建模器
-    mounted() {
-        this.init();
+    handleChange(file) {
+      console.log(file, "fileList");
+      let currentFile = file.file;
+      this.openBpmn(currentFile);
+    },
+    openBpmn(file) {
+      console.log(file, "file");
+      const reader = new FileReader();
+      // 读取File对象中的文本信息，编码格式为UTF-8
+      reader.readAsText(file.originFileObj, "utf-8");
+      reader.onload = () => {
+        //读取完毕后将文本信息导入到Bpmn建模器
+        this.createNewDiagram(reader.result);
+        console.log("reader.result", reader.result + "");
+      };
+      return false;
+    },
+    newDiagram() {
+      this.createNewDiagram(this.bpmnTemplate);
+    },
+    downloadBpmn() {
+      this.bpmnModeler.saveXML({ format: true }, (err, xml) => {
+        if (!err) {
+          console.log(xml, "xml");
+          // 获取文件名
+          const name = `${this.getFilename(xml)}.bpmn`;
+          // 将文件名以及数据交给下载函数
+          this.download({ name: name, data: xml });
+        }
+      });
+    },
+    downloadSvg() {
+      this.bpmnModeler.saveXML({ format: true }, (err, xml) => {
+        if (!err) {
+          // 获取文件名
+          const name = `${this.getFilename(xml)}.svg`;
+
+          // 从建模器画布中提取svg图形标签
+          let context = "";
+          const djsGroupAll = this.$refs.canvas.querySelectorAll(".djs-group");
+          for (let item of djsGroupAll) {
+            context += item.innerHTML;
+          }
+          // 获取svg的基本数据，长宽高
+          const viewport = this.$refs.canvas
+            .querySelector(".viewport")
+            .getBBox();
+
+          // 将标签和数据拼接成一个完整正常的svg图形
+          const svg = `
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="${viewport.width}"
+          height="${viewport.height}"
+          viewBox="${viewport.x} ${viewport.y} ${viewport.width} ${viewport.height}"
+          version="1.1"
+          >
+          ${context}
+        </svg>
+      `;
+          // 将文件名以及数据交给下载函数
+          this.download({ name: name, data: svg });
+        }
+      });
+    },
+    download({ name = "diagram.bpmn", data }) {
+      const blob = new Blob([data], { type: "data:text/xml;charset=utf-8" });
+      console.log(name);
+      var a = document.createElement("a");
+      var href = window.URL.createObjectURL(blob);
+      a.href = href;
+      a.download = name;
+      document.body.appendChild(a);
+      a.click();
+      window.URL.revokeObjectURL(href);
+      // console.log('--------------',blob);
+      // // 这里就获取到了之前设置的隐藏链接
+      // const downloadLink = this.$refs.downloadLink;
+      // // 把数据转换为URI，下载要用到的
+      // const encodedData = encodeURIComponent(data,{type:'data:text/xml;charset=utf-8'});
+      // if (data) {
+      //     // 将数据给到链接
+      //     downloadLink.href =
+      //         "data:application/bpmn20-xml;charset=UTF-8," + encodedData;
+      //     // 设置文件名
+      //     downloadLink.download = name;
+      //     // 触发点击事件开始下载
+      //     downloadLink.click();
+      // }
+    },
+
+    getFilename(xml) {
+      let start = xml.indexOf("process");
+      let filename = xml.substr(start, xml.indexOf(">"));
+      filename = filename.substr(filename.indexOf("id") + 4);
+      filename = filename.substr(0, filename.indexOf('"'));
+      return filename;
     }
+  },
+  // 生命周期钩子，在组件加载完成后调用init函数进行创建初始化Bpmn-js建模器
+  mounted() {
+    this.init();
+  }
 };
 </script>
 <style scoped>
 .container {
-    width: 100%;
-    height: calc(100vh - 200px);
-    display: flex;
+  width: 100%;
+  height: calc(100vh - 200px);
+  display: flex;
+}
+.tool {
+  display: flex;
 }
 
 .bpmn-canvas {
-    height: 100%;
-    width: 80vw;
+  height: 100%;
+  width: 80vw;
 }
 
 .bpmn-js-properties-panel {
-    flex: 1;
+  flex: 1;
 }
->>> .bjs-powered-by{
-    display: none;
+/deep/ .bjs-powered-by {
+  display: none;
+}
+/deep/.ant-upload-list,
+.ant-upload-list-text {
+  display: none;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -8,35 +8,53 @@
 //     },
 // }
 module.exports = {
-  publicPath: "./", // 公共路径(必须有的)
-  outputDir: "dist", // 输出文件目录
-  assetsDir: "static", //静态资源文件名称
-  lintOnSave: false,
-  productionSourceMap: false, //去除打包后js的map文件
-  devServer:{
-      // proxy:{
-      //     '/api':{
-      //         changeOrigin:true,
-      //         target: 'http://172.24.148.199:7003',  // 后台接口域名
-      //         ws: true,        //如果要代理 websockets，配置这个参数
-      //         // secure: false,  // 如果是https接口，需要配置这个参数
-      //         changeOrigin: true,  //是否跨域
-      //         pathRewrite : {
-      //             '^/api':''
-      //         }
-      //     }
-      // }
-      
-},
-  //去掉console
-  configureWebpack: (config) => {
-      // 判断为生产模式下，因为开发模式我们是想保存console的
-      if (process.env.NODE_ENV === "production") {
-          config.optimization.minimizer.map((arg) => {
-              const option = arg.options.terserOptions.compress;
-              option.drop_console = true; // 打开开关
-              return arg;
-          });
-      }
-  },
+    publicPath: "./", // 公共路径(必须有的)
+    outputDir: "dist", // 输出文件目录
+    assetsDir: "static", //静态资源文件名称
+    lintOnSave: false,
+    productionSourceMap: false, //去除打包后js的map文件
+    devServer: {
+        // proxy:{
+        //     '/api':{
+        //         changeOrigin:true,
+        //         target: 'http://172.24.148.199:7003',  // 后台接口域名
+        //         ws: true,        //如果要代理 websockets，配置这个参数
+        //         // secure: false,  // 如果是https接口，需要配置这个参数
+        //         changeOrigin: true,  //是否跨域
+        //         pathRewrite : {
+        //             '^/api':''
+        //         }
+        //     }
+        // }
+
+    },
+    //去掉console
+    configureWebpack: (config) => {
+        config.resolve.alias
+            .set('@', resolve('src'))
+            .set('@assets', resolve('src/assets'))
+            .end()
+        config.module // 主要是看这部分
+            .rule('xml-loader')
+            .test(/.(bpmn|xml)$/)
+            .use('xml-loader')
+            .loader('xml-loader')
+            .end()
+
+        // 判断为生产模式下，因为开发模式我们是想保存console的
+        if (process.env.NODE_ENV === "production") {
+            config.optimization.minimizer.map((arg) => {
+                const option = arg.options.terserOptions.compress;
+                option.drop_console = true; // 打开开关
+                return arg;
+            });
+        }
+    },
+    css: {
+        loaderOptions: {
+            sass: {
+                prepenData: `@import ~@/assets/scss/index.css`
+            }
+        }
+    },
 };
